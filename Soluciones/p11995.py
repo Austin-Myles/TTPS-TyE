98fcaada5cc9a583f5fd758da7fdba48c56ae040
Austin <austin.myles192994@alumnos.info.unlp.edu.ar>
2024-09-26 21:42:19 -0300

    Nuevos ejercicios para revisar en clase ;p

--- Soluciones/p11995.py
+++ Soluciones/p11995.py
@@ -1,33 +1,59 @@
+import heapq
+
+# RUNTIME ERROR
 
 while True:
     try:
         n = int(input())
-        aux_queue = []
-        aux_cola = []
-        aux_prio = []
-        cola = True
-        pila = True
-        prio = True
+
+        pila = []
+        cola = []
+        prio = []
+
+        es_cola = True
+        es_pila = True
+        es_prio = True
+
         for x in range(n):
             op = list(map(int, input().split()))
-        
+            
             if op[0] == 1:
-                if cola:
-                    aux_queue.append(op[1])
+                pila.append(op[1])
+                cola.append(op[1])
+                heapq.heappush(prio, - op[1])
+
+            elif op[0] == 2:
+                if pila:
+                    if op[1] != pila.pop():
+                        es_pila = False
+                else:
+                    es_pila = False
                 
+                if cola:
+                    if op[1] != cola.pop(0):
+                        es_cola = False
+                else:
+                    es_cola = False
+                
+                if prio:
+                    if op[1] != -heapq.heappop(prio):
+                        es_prio = False
+                else:
+                    es_prio = False
+            
+        posibilidad = es_prio + es_cola + es_pila
 
-
-        """
-        if(cola and (not pila and not prio)):
-            print("queue")
-        elif(pila and (not cola and not prio)):
-            print("stack")
-        elif(prio and (not cola and not pila)):
-            print("priority queue")
-        elif(not prio and (not cola and not pila)):
+        if posibilidad == 0:
             print("imposible")
-        else:
+        elif posibilidad > 1:
             print("not sure")
-        """
+        else:
+            if es_cola:
+                print("queue")
+            elif es_pila:
+                print("stack")
+            else:
+                print("priority queue")
+                
     except(EOFError):
         break
